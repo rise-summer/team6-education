51efb76b70514e32916cc228a1821035ac5b9f29
Christie Ruales <cruales@ufl.edu>
2020-08-03 11:17:13 -0400

    Add class page, student assignment list, and assignment page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StudentAssignments from './Classes/StudentAssignments';
+import Assignment from './Classes/Assignment';
+import ClassPage from './Classes/ClassPage';
+import {Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/StudentAssignments" component={StudentAssignments}/>
+        <Route exact path="/Assignment" component={Assignment}/>
+        <Route exact path="/ClassPage" component={ClassPage}/>
+      </Switch>
+      
     </div>
   );
 }
